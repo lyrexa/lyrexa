81c2843b23f9fd3b9f52286a7754eefd227ab53f
Oreo <thediamondgamer331@gmail.com>
2022-06-11 03:43:24 +0100

    Finished index completely

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ let slashCommands = [];
 
 const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = require(`./commands/${file}`).command;
     client.commands.set(command.name, command);
     let slash = command.data ? command.data : null;
     if (slash) slashCommands.push(slash);
@@ -20,7 +20,7 @@ for (const file of commandFiles) {
 
 const eventFiles = readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
+    const event = require(`./events/${file}`).event;
     const eventName = file.split('.')[0];
     client.on(eventName, event.bind(null, client));
 }
