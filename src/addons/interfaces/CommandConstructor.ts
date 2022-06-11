efa397df44da2dea5712325ce2ef7c74c69a7d4b
Oreo <thediamondgamer331@gmail.com>
2022-06-11 17:48:09 +0100

    type changes

--- src/addons/interfaces/CommandConstructor.ts
+++ src/addons/interfaces/CommandConstructor.ts
@@ -14,13 +14,22 @@ export interface CommandConstructor {
 
 export interface HelpData {
     show: boolean;
-    category: string;
-
+    category: "info" | "mod" | "fun" | "eco" | "other";
+    showDev?: boolean;
     dev?: DevHelpData;
 }
 
 export interface DevHelpData {
     show: boolean;
-    category: string;
-    level?: number;
+    category: "dev" | "support" | "admin";
+    level?: 1 | 2 | 3 | 4 | 5;
 }
+
+/*
+DevHelpData - Level
+1 - Shows up for anyone on the staff
+2 - Shows only for support staff+
+3 - Shows only for botadmins
+4 - Shows only for BTMC Team members
+5 - Shows only for Developers
+*/
