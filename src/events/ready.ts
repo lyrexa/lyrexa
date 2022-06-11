ada47de545ad5af1214983c8160d3f0456540eda
Oreo <thediamondgamer331@gmail.com>
2022-06-11 03:57:28 +0100

    Added the return of void to ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,5 @@
 import { Client } from '../addons/classes/ModdedClient';
 
-export function event(client: Client) {
+export function event(client: Client): void {
     console.log(`${client.user?.tag} is online!`);
 }
