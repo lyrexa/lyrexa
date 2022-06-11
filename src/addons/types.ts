48ec2d4351426e3c16ed725152000e4bfc374092
Oreo <thediamondgamer331@gmail.com>
2022-06-11 17:28:11 +0100

    export the new types

--- src/addons/types.ts
+++ src/addons/types.ts
@@ -1,7 +1,9 @@
-import { CommandConstructor as commandconstructor } from "./interfaces/CommandConstructor";
+import { CommandConstructor as commandconstructor, HelpData, DevHelpData } from "./interfaces/CommandConstructor";
 import { Client as client } from "./classes/ModdedClient";
 import { ClientOptions as clientoptions } from "./interfaces/ClientOptions";
 
 export type CommandConstructor = commandconstructor;
 export type Client = client;
 export type ClientOptions = clientoptions;
+export type HelpMenuData = HelpData;
+export type DevHMenuData = DevHelpData;
