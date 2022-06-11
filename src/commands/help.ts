49a590de36e0728b78b3c80e25f50a3a5aca6846
Oreo <thediamondgamer331@gmail.com>
2022-06-12 00:00:13 +0100

    Add the ability to run /help on a command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,4 @@
-import { Client, CommandConstructor, HelpMenus } from "../addons/types";
+import { Client, CommandConstructor, AHD_META_DATA } from "../addons/types";
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { MessageActionRow, MessageEmbed, MessageSelectMenu } from "discord.js";
 import { advanced as advancedHelpData } from "./commands_advanced/help.advanced";
@@ -24,6 +24,11 @@ const data: CommandConstructor = {
                     { name: 'Developer', value: 'dev' }
                 )
         )
+        .addStringOption(opt =>
+            opt.setName('command')
+                .setDescription('The command to get more info')
+                .setRequired(false)
+        )
     ,
     help: {
         show: true,
@@ -34,6 +39,56 @@ const data: CommandConstructor = {
     interaction: (client, interaction) => {
         // Get category
         const category = interaction.options.getString('category');
+        const command = interaction.options.getString('command');
+
+        if (category && command) {
+            interaction.reply({ content: 'You can only view a command or a category at once', ephemeral: true });
+            return;
+        }
+
+        if (command) {
+            // Command code
+            const command_: CommandConstructor = client.commands.find(cmd => cmd.name == command.toLowerCase());
+            if (!command_) {
+                interaction.reply({ content: 'Error | Failed to find command ' + command, ephemeral: true });
+                return;
+            }
+
+            const advanced = command_.help.advanced;
+
+            const embed = new MessageEmbed()
+                .setTitle(`${client.user?.username} Help - ${capitalizeFirstLetter(command_.name)}`)
+                .setDescription("Here is all the information I can find about the " + command_.name + " command.\n[] optional, {} required")
+            //
+
+            if (!advanced) {
+                embed.addField('Category', command_.help.category, true);
+
+                interaction.reply({ content: 'Warn | This command does not have an cmd.help.advanced property', embeds: [embed], ephemeral: true });
+
+                return;
+            }
+
+            const fields: FVersion[] = [];
+
+            fields.push({ name: 'Usage [Slash]', value: generateUsage(advanced.usage.slash, command_.name), inline: true });
+
+            fields.push({ name: 'Usage [Slash]', value: generateUsage(advanced.usage.message, command_.name), inline: true });
+
+            if (advanced.parameters) fields.push({ name: 'Parameters', value: solveParameters(advanced.parameters), inline: true });
+
+            fields.push({ name: 'Category', value: command_.help.category, inline: true });
+
+            if (advanced.commandDetails) fields.push({ name: 'Command Details', value: convertCommandDetails(advanced.commandDetails), inline: true });
+
+            if (advanced.codeDetails) fields.push({ name: 'Code Details', value: convertCodeDetails(advanced.codeDetails, client), inline: true });
+
+            embed.setFields(fields);
+
+            interaction.reply({ embeds: [embed], ephemeral: true });
+
+            return;
+        }
 
         // Error if dev page
         if (category == 'dev') {
@@ -154,6 +209,86 @@ function capitalizeFirstLetter(string: string): string {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function convertCommandDetails(details: AHD_META_DATA._AHD_CommandDetails): string {
+    let result = '';
+
+    if (details.level == 'Top') {
+        result += 'Top Level Command\n';
+    }
+
+    if (details.level == 'Sub') {
+        result += 'Sub Command\n';
+    }
+
+    result += `Options: ${details.optionsCount}`;
+
+    return result;
+}
+
+function convertCodeDetails(details: AHD_META_DATA._AHD_CodeDetails, client: Client): string {
+    let result = '';
+
+    if (details.lines) result += `Lines: ${details.lines}\n`;
+
+    result += `Contributors: ${details.contributors.count}\n`;
+
+    const people = details.contributors.people;
+
+    let out: PSN[] = [];
+
+    people.forEach(async person => {
+        let obj: PSN = {
+            ping: `<@!${person.id}>`,
+            tag: 'null'
+        }
+        const d = client.users.cache.get(person.id) || await client.users.fetch(person.id).catch(console.log);
+
+        if (d) obj.tag = d.tag;
+        else obj.tag = person.data.name + '#' + person.data.discriminator + '?'
+
+        out.push(obj);
+    })
+
+    out.forEach(async person => {
+        let string = `- ${person.ping} | ${person.tag}`;
+
+        if (out.indexOf(person) == out.length - 1) string += '\n';
+
+        result += string;
+    })
+
+    return result;
+}
+
+function generateUsage(inp: AHD_META_DATA._AHD_UsageItem[], cmdName: string): string {
+    let string = '';
+
+    inp.forEach(item => {
+        let str = `/${cmdName}`;
+
+        if (item.options) {
+            item.options.forEach(option => {
+                if (option.required) str += ` {${option.name}}`;
+                else str += ` [${option.name}]`;
+            })
+        }
+
+        string += `${str}\n`;
+    })
+
+    return string;
+}
+
+function solveParameters(parameters: AHD_META_DATA._AHD_Parameter[]): string {
+    let string = '';
+
+    parameters.forEach(parameter => {
+        string += `${parameter.name}: ${parameter.description}\n`;
+    })
+
+    return string;
+}
+
 interface command {
     name: string;
     description: string;
@@ -164,4 +299,9 @@ interface FVersion {
     name: string;
     value: string;
     inline: boolean;
+}
+
+interface PSN {
+    ping: string;
+    tag: string;
 }
