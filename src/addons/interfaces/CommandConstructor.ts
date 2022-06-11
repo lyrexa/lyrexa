a9350de3716d4812e93f33ae6ee918c02ac2438a
Oreo <thediamondgamer331@gmail.com>
2022-06-11 17:28:00 +0100

    Add help menu data

--- src/addons/interfaces/CommandConstructor.ts
+++ src/addons/interfaces/CommandConstructor.ts
@@ -6,7 +6,21 @@ export interface CommandConstructor {
     name: string;
     description: string;
     data: SlashCommandBuilder;
+    help: HelpData;
     interaction(client: Client, interaction: CommandInteraction): void;
     message(client: Client, message: Message, args: string[]): void;
     nonCmdInteraction(client: Client, interaction: Interaction): void;
+}
+
+export interface HelpData {
+    show: boolean;
+    category: string;
+
+    dev?: DevHelpData;
+}
+
+export interface DevHelpData {
+    show: boolean;
+    category: string;
+    level?: number;
 }
