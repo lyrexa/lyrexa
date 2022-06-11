3d3e8e6f07a54143b0f015270bec2eb0209deea8
Oreo <thediamondgamer331@gmail.com>
2022-06-11 16:55:26 +0100

    Interaction -> CommandInteraction

--- src/addons/interfaces/CommandConstructor.ts
+++ src/addons/interfaces/CommandConstructor.ts
@@ -1,12 +1,12 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { Interaction, Message } from 'discord.js';
+import { CommandInteraction, Interaction, Message } from 'discord.js';
 import { Client } from '../types';
 
 export interface CommandConstructor {
     name: string;
     description: string;
     data: SlashCommandBuilder;
-    interaction(client: Client, interaction: Interaction): void;
+    interaction(client: Client, interaction: CommandInteraction): void;
     message(client: Client, message: Message, args: string[]): void;
     nonCmdInteraction(client: Client, interaction: Interaction): void;
 }
